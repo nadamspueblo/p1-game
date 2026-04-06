0716b0bc6e3bd12d5245cf1b7b3926eb17495f24
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2026-04-06 15:48:49 +0000

    Day 2

--- script.js
+++ script.js
@@ -4,22 +4,60 @@ function start() {
     console.log("click");
 
     // Start level 1
-    createCircle();
+    let num = getRandomNum();
+    createCircle(num);
+    createCircle(num);
+    num = getRandomNum();
+    createCircle(num);
+    createCircle(num);
+    num = getRandomNum();
+    createCircle(num);
+    createCircle(num);
+    num = getRandomNum();
+    createCircle(num);
+    createCircle(num);
+
 }
 
-function createCircle() {
+function createCircle(num) {
     // Create a div for the circle
     let circle = document.createElement("div");
 
     //  Set text to a number
-    circle.innerText = "67";
+    circle.innerText = num;
 
     // Set color, shape, font size, etc
     circle.style.backgroundColor = "red";
     circle.style.width = "100px";
     circle.style.height = "100px";
+    circle.style.borderRadius = "50%";
+    circle.style.textAlign = "center";
+    circle.style.alignContent = "center";
+    circle.style.color = "white";
+    circle.style.fontSize = "30px";
+    circle.style.position = "absolute";
+    circle.style.left = getRandomX() + "px";
+    circle.style.top = getRandomY() + "px";
 
     // Add the circle to the page
     document.body.appendChild(circle);
 
+}
+
+// Generates random x coordinates
+function getRandomX() {
+    let randX = Math.random() * window.innerWidth; 
+    return randX;
+}
+
+// Generates a random y coordinate
+function getRandomY() {
+    let randY = Math.random() * window.innerHeight;
+    return randY;
+}
+
+function getRandomNum() {
+    let rand = Math.random() * 100;
+    rand = Math.floor(rand);
+    return rand;
 }
